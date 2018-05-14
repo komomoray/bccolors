5b45ef6b2f04dce7a8e1b4139b876cb573a5276d
tecking <tets.morikawa@gmail.com>
2018-05-14 10:02:09 +0900

    メールプラグイン テンプレートファイルのJS・CSSパス指定修正

--- Mail/default/submit.php
+++ Mail/default/submit.php
@@ -18,7 +18,7 @@ if (Configure::read('debug') == 0) {
 	<p>お問い合わせ頂きありがとうございました。<br />
 		確認次第、ご連絡させて頂きます。</p>
 	<?php if ($mailContent['MailContent']['redirect_url']): ?>
-		<p>※５秒後にトップページへ自動的に移動します。</p>
+		<p>※5秒後にトップページへ自動的に移動します。</p>
 		<p> <a href="<?php echo $mailContent['MailContent']['redirect_url'] ?>">移動しない場合はコチラをクリックしてください。≫</a> </p>
 	<?php endif; ?>
 </div>
